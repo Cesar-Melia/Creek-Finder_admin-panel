1add3d4b936bc8946d30e09ea29b8458824393f3
frank <frankestrada41@hotmail.com>
2021-07-07 21:23:13 +0200

    FEAT: home routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  { path: '', loadChildren: () => import('./pages/home/home.module').then((m) => m.HomeModule) },
   { path: 'creek', loadChildren: () => import('./pages/creek/creek.module').then((m) => m.CreekModule) }
 ];
 
