1f2024361a922ef33f089fcf46c4ed2b4f5dc829
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-08 23:35:04 +0200

    FEAT:header component and mobile menu

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  menuActive: boolean = false;
+  isAuth: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  activateMenu(): void {
+    this.menuActive = !this.menuActive;
+
+    console.log('menu -->', this.menuActive);
   }
-
 }
