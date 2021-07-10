b5fd22357836ddc6f356744a96ffe5e9a3224070
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-10 13:25:27 +0200

    FEAT: form in login created

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from 'src/app/shared/services/auth.service';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss'],
+  // providers: [AuthService],
 })
 export class LoginComponent implements OnInit {
-  constructor(private authService: AuthService) {}
+  loginForm: any;
+
+  constructor(
+    private authService: AuthService,
+    private formBuilder: FormBuilder
+  ) {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
 
   ngOnInit(): void {}
 
-  //Comprobar si se puede usar directamente.
-  login(email: string, password: string): void {
-    this.authService.login(email, password).subscribe((loginData: any) => {
-      console.log('Usuario logueado: ', loginData);
-    });
+  submitForm(): void {
+    console.log('Campos formulario: ', this.loginForm);
+    console.log('Campo email: ', this.loginForm.email);
+
+    this.authService
+      .login(this.loginForm.email, this.loginForm.password)
+      .subscribe((loginData: any) => {
+        console.log('Usuario logueado: ', loginData);
+      });
   }
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -20,5 +20,5 @@ export class AuthService {
     return this.http.post(`${this.API_URL}/login`, body);
   }
 
-  //logout
+  ///logout
 }
