82dbde867bebc3ebc29aaa0060c7d7908a1a74ac
Cesar Melia <cesarmeliaherraiz@gmail.com>
2021-07-12 06:08:10 +0200

    REFACTOR: comments edit and create components and routes deleted

--- src/app/pages/comments/comments-routing.module.ts
+++ src/app/pages/comments/comments-routing.module.ts
@@ -1,14 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CommentsComponent } from './comments.component';
-import { CreateCommentComponent } from './pages/create-comment/create-comment.component';
 import { DeleteCommentComponent } from './pages/delete-comment/delete-comment.component';
-import { UpdateCommentComponent } from './pages/update-comment/update-comment.component';
 
 const routes: Routes = [
   { path: '', component: CommentsComponent },
-  { path: 'create-comment', component: CreateCommentComponent },
-  { path: 'update-comment', component: UpdateCommentComponent },
   { path: 'delete-comment', component: DeleteCommentComponent },
 ];
 
--- src/app/pages/comments/comments.module.ts
+++ src/app/pages/comments/comments.module.ts
@@ -3,19 +3,13 @@ import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 
 import { CommentsRoutingModule } from './comments-routing.module';
-import { CreateCommentComponent } from './pages/create-comment/create-comment.component';
-import { UpdateCommentComponent } from './pages/update-comment/update-comment.component';
+
 import { DeleteCommentComponent } from './pages/delete-comment/delete-comment.component';
 import { CommentsComponent } from './comments.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    CreateCommentComponent,
-    UpdateCommentComponent,
-    DeleteCommentComponent,
-    CommentsComponent,
-  ],
+  declarations: [DeleteCommentComponent, CommentsComponent],
   imports: [CommonModule, CommentsRoutingModule, HttpClientModule, FormsModule],
 })
 export class CommentsModule {}
